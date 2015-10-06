4836c9dcf68be609ce6512d7bce31174f66055cf
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-10-06 19:42:04 +0200

    minor

--- src/testScriptAll.sh
+++ src/testScriptAll.sh
@@ -5,5 +5,5 @@
  echo "TESTING full_passport_eq (grep 'cannot):" &&
  ./proverif -in pitype -ukano ./examples/full_passport_eq.pi && ./proverif -in pitype OUTPUT_C2.pi | grep cannot &&
  echo "TESTING AKA (grep 'cannot):" &&
- ./proverif -in pitype -ukano ./examples/aka.pi && ./proverif -in pitype OUTPUT_C2.pi | grep cannot
+ ./proverif -in pitype -ukano ./examples/AKA.pi && ./proverif -in pitype OUTPUT_C2.pi | grep cannot
 
