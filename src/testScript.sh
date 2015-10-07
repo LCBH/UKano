5a1c0e7b68c9d4a21e2da35b8738804fc17cbbe3
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-10-07 22:28:07 +0200

    better script test

--- src/testScript.sh
+++ src/testScript.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
  make && 
- echo "TESTING PACE (grep 'cannot):";
- ./proverif -in pitype -ukano ./examples/pace.pi && ./proverif -in pitype OUTPUT_C2.pi | grep cannot
+ echo "TESTING full_passport_eq:";
+ ./proverif -in pitype -ukano ./examples/full_passport_eq.pi
+ echo "Cannot prove:";
+ ./proverif -in pitype OUTPUT_C2.pi | grep cannot
+ echo "False:";
+ ./proverif -in pitype OUTPUT_C2.pi | grep false
+ echo "True:";
+ ./proverif -in pitype OUTPUT_C2.pi | grep true
